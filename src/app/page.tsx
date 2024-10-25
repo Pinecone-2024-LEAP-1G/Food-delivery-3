5103d11c6b74809c1283629421fbd468a08a0ce9
24LP4300@nest.edu.mn <24lp4300@G305-CC0072.local>
2024-10-25 12:26:41 +0800

    conflict

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,10 @@
-import { EditableCard } from "@/components/EditableCard";
+import { EditableCard } from '@/components/EditableCard';
 
 const Home = () => {
   return (
-<<<<<<< HEAD
     <div>
       <EditableCard />
     </div>
-=======
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20"></div>
->>>>>>> ce02594 (berhee-final-header-footer)
   );
 };
 export default Home;
--- src/components/ForgotStepThree.tsx
+++ src/components/ForgotStepThree.tsx
@@ -1,9 +1,9 @@
-"use client";
+'use client';
 
-import { z } from "zod";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
-import { Button } from "@/components/ui/button";
+import { z } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import { Button } from '@/components/ui/button';
 import {
   Form,
   FormControl,
@@ -11,29 +11,29 @@ import {
   FormItem,
   FormLabel,
   FormMessage,
-} from "@/components/ui/form";
-import { Input } from "@/components/ui/input";
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
 
 const formSchema = z
   .object({
     password: z
       .string()
-      .min(8, "Нууц үг хамгийн багадаа 8 тэмдэгт байх ёстой.")
-      .max(50, "Нууц үг хамгийн ихдээ 50 тэмдэгт байх ёстой.")
-      .regex(/[A-Z]/, "Нууц үг нэг том үсэг агуулсан байх ёстой.")
-      .regex(/[0-9]/, "Нууц үг нэг тоо агуулсан байх ёстой."),
+      .min(8, 'Нууц үг хамгийн багадаа 8 тэмдэгт байх ёстой.')
+      .max(50, 'Нууц үг хамгийн ихдээ 50 тэмдэгт байх ёстой.')
+      .regex(/[A-Z]/, 'Нууц үг нэг том үсэг агуулсан байх ёстой.')
+      .regex(/[0-9]/, 'Нууц үг нэг тоо агуулсан байх ёстой.'),
 
     repassword: z
       .string()
-      .min(8, "Нууц үг хамгийн багадаа 8 тэмдэгт байх ёстой.")
-      .max(50, "Нууц үг хамгийн ихдээ 50 тэмдэгт байх ёстой."),
+      .min(8, 'Нууц үг хамгийн багадаа 8 тэмдэгт байх ёстой.')
+      .max(50, 'Нууц үг хамгийн ихдээ 50 тэмдэгт байх ёстой.'),
   })
   .superRefine((data, ctx) => {
     if (data.password !== data.repassword) {
       ctx.addIssue({
-        path: ["repassword"],
+        path: ['repassword'],
         code: z.ZodIssueCode.custom,
-        message: "Нууц үг давхцаж байх ёстой.",
+        message: 'Нууц үг давхцаж байх ёстой.',
       });
     }
   });
@@ -46,8 +46,8 @@ export const ForgotStepThree = ({ handleNextStep }: Type) => {
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
-      password: "",
-      repassword: "",
+      password: '',
+      repassword: '',
     },
   });
 
@@ -57,13 +57,11 @@ export const ForgotStepThree = ({ handleNextStep }: Type) => {
   }
 
   return (
-<<<<<<< HEAD
     <div>
       <Form {...form}>
         <form
           onSubmit={form.handleSubmit(onSubmit)}
-          className="flex flex-col gap-3"
-        >
+          className="flex flex-col gap-3">
           <FormField
             control={form.control}
             name="password"
@@ -76,7 +74,7 @@ export const ForgotStepThree = ({ handleNextStep }: Type) => {
                     placeholder="Нууц үг оруулна уу"
                     type="password"
                     {...field}
-                    onFocus={() => form.clearErrors("password")}
+                    onFocus={() => form.clearErrors('password')}
                   />
                 </FormControl>
                 <FormMessage />
@@ -95,7 +93,7 @@ export const ForgotStepThree = ({ handleNextStep }: Type) => {
                     placeholder="Нууц үг дахин оруулна уу"
                     type="password"
                     {...field}
-                    onFocus={() => form.clearErrors("repassword")}
+                    onFocus={() => form.clearErrors('repassword')}
                   />
                 </FormControl>
                 <FormMessage />
@@ -105,47 +103,11 @@ export const ForgotStepThree = ({ handleNextStep }: Type) => {
           <Button
             className="mt-8 h-12 w-full bg-[#F7F7F8] rounded"
             variant="outline"
-            type="submit"
-          >
+            type="submit">
             Үргэлжлүүлэх
           </Button>
         </form>
       </Form>
-=======
-    <div className="flex flex-col gap-2  ">
-      <div className="grid grid-cols w-full max-w-sm items-center gap-1.5 justify-center">
-        <Label htmlFor="email">Нууц үг</Label>
-        <div className="flex h-12 w-[384px] p-2 bg-[#F7F7F8] items-center justify-center border border-[#ECEDF0] rounded">
-          <Input
-            className="bg-[#F7F7F8] grow border-none"
-            type={showPassword ? 'text' : 'password'}
-            id="email"
-            placeholder="Нууц үг оруулна уу"
-          />
-          <button
-            onClick={togglePasswordVisibility}
-            className="ml-auto flex items-center justify-center">
-            {showPassword ? <FaEye /> : <FaEyeSlash />}
-          </button>
-        </div>
-      </div>
-      <div className="grid grid-cols w-full max-w-sm items-center gap-1.5 justify-center">
-        <Label htmlFor="email">Нууц үг дахин</Label>
-        <div className="flex h-12 w-[384px] p-2 bg-[#F7F7F8] items-center justify-center border border-[#ECEDF0] rounded">
-          <Input
-            className="bg-[#F7F7F8] grow border-none"
-            type={showPassword ? 'text' : 'password'}
-            id="email"
-            placeholder="Нууц үг дахин оруулна уу"
-          />
-          <button
-            onClick={togglePasswordVisibility}
-            className="ml-auto flex items-center justify-center">
-            {showPassword ? <FaEye /> : <FaEyeSlash />}
-          </button>
-        </div>
-      </div>
->>>>>>> 9921d94 (berhee-final-header-footer)
     </div>
   );
 };
