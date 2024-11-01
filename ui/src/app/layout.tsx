61a0f899a3462de06696e1489483efd3a18b178e
24LP8078 24LP8078 <24LP8078@G305-CC0038.local>
2024-11-01 12:16:02 +0800

    eslint

--- ui/src/app/layout.tsx
+++ ui/src/app/layout.tsx
@@ -2,11 +2,11 @@ import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 
-export default function RootLayout({
+export const RootLayout = ({
   children,
 }: Readonly<{
   children: React.ReactNode;
-}>) {
+}>) => {
   return (
     <html lang="en">
       <body className="flex flex-col min-h-screen">
@@ -16,4 +16,4 @@ export default function RootLayout({
       </body>
     </html>
   );
-}
+};
