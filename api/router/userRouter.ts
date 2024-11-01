e2a62a93e0c5fcf23a3187d3d22401af1ddb21c9
24LP4300@nest.edu.mn <24lp4300@G305-CC0072.local>
2024-11-01 12:49:33 +0800

    crete-order-model-hiiv

--- api/router/userRouter.ts
+++ api/router/userRouter.ts
@@ -1,14 +1,14 @@
-import express from "express";
-import { CreateUser } from "../controller/users/createUser";
-import { updateUser } from "../controller/users/updateUser";
-import { deleteUser } from "../controller/users/deleteUser";
-import { GetUsers } from "../controller/users/getUsers";
-import { GetUser } from "../controller/users/getUser";
+import express from 'express';
+import { CreateUser } from '../controller/users/createUser';
+import { updateUser } from '../controller/users/updateUser';
+import { deleteUser } from '../controller/users/deleteUser';
+import { GetUsers } from '../controller/users/getUsers';
+import { GetUser } from '../controller/users/getUser';
 
 export const UserRouter = express.Router();
 
-UserRouter.post("/createUser", CreateUser);
-UserRouter.get("/getUsers", GetUsers);
-UserRouter.get("/getUser", GetUser);
-UserRouter.put("/updateUser", updateUser);
-UserRouter.delete("/deleteUser", deleteUser);
+UserRouter.post('/createUser', CreateUser);
+UserRouter.get('/getUsers', GetUsers);
+UserRouter.get('/getUser', GetUser);
+UserRouter.put('/updateUser', updateUser);
+UserRouter.delete('/deleteUser', deleteUser);
