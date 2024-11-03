075f166432f631e4d621473461829e212ee8c1d0
Tuul10 <etuul186@gmial.com>
2024-11-03 13:52:57 +0800

    bcrypt

---
+++ api/controller/users/sign-in.ts
@@ -0,0 +1,25 @@
+import { Request, Response } from "express";
+import UserModel from "../../model/user";
+import bcrypt from "bcrypt";
+
+export const sigIn = async (req: Request, res: Response) => {
+  const { email, password } = req.body;
+  try {
+    const user = await UserModel.findOne({ email });
+    if (!user) {
+      res.status(400).json({ message: "" });
+      return;
+    }
+
+    const isMatchedPassword = bcrypt.compare(password, user.password);
+
+    if (!isMatchedPassword) {
+      res.status(400).json({ message: "email eswel pass buruu baina" });
+      return;
+    }
+
+    res.json(user);
+  } catch (error) {
+    res.send(error);
+  }
+};
--- api/controller/users/sign-up.ts
+++ api/controller/users/sign-up.ts
@@ -5,15 +5,17 @@ import bcrypt from "bcrypt";
 export const CreateUser = async (req: Request, res: Response) => {
   const { userName, email, address, password, role } = req.body;
   const saltRounds = 10;
+  console.log(req.body);
+
   try {
     const hashedPassword = await bcrypt.hash(password, saltRounds);
 
     const user = await new UserModel({
-      userName,
-      email,
-      address,
+      userName: userName,
+      email: email,
+      address: address,
       password: hashedPassword,
-      role,
+      role: role,
     }).save();
 
     res.json({ user: user });
