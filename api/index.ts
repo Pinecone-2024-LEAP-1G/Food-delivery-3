967f84081491eb8bced2d7fa2e4a80844c3f1b04
24LP4300@nest.edu.mn <24lp4300@G305-CC0072.local>
2024-11-04 13:00:35 +0800

    crud-conflict-fixed

--- api/index.ts
+++ api/index.ts
@@ -1,16 +1,9 @@
 require('dotenv').config();
 
-<<<<<<< HEAD
-import { connectDb } from "./database";
-import { categoryRouter } from "./router/categoryRouter";
-import { UserRouter } from "./router/userRouter";
-import express from "express";
-=======
 import { connectDb } from './database';
 import { OrderRouter } from './router/OrderRouter';
 import { UserRouter } from './router/userRouter';
 import express from 'express';
->>>>>>> 47-order-crud
 
 const app = express();
 
@@ -18,13 +11,8 @@ app.use(express.json());
 
 const port = 8000;
 
-<<<<<<< HEAD
-app.use("/", UserRouter);
-app.use("/", categoryRouter);
-=======
 app.use('/users', UserRouter);
 app.use('/orders', OrderRouter);
->>>>>>> 47-order-crud
 
 const server = async () => {
   await connectDb();
