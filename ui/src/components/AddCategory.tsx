47562fcc1af68d9667b86c64fe9c90e2fb1806ca
24LP8078 24LP8078 <24LP8078@admins-iMac.local>
2024-11-07 23:02:57 -0800

    category front

--- ui/src/components/AddCategory.tsx
+++ ui/src/components/AddCategory.tsx
@@ -2,20 +2,43 @@ import { Button } from "@/components/ui/button";
 import {
   Dialog,
   DialogContent,
-  DialogFooter,
   DialogHeader,
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import axios from "axios";
+import { useState } from "react";
+import { toast } from "sonner";
 
 export const AddCategory = () => {
+  const [name, setName] = useState("");
+
+  const getCategory = async () => {
+    try {
+      const category = await axios.post("http://localhost:8000/category", {
+        categoryName: name,
+      });
+      toast.success("amjilttai burtgegdlee");
+      setName("");
+      console.log(category);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const clickAddCategory = () => {
+    if (!name) {
+      toast.error("category ner hooson baina ");
+    } else getCategory();
+  };
+
   return (
     <Dialog>
-      <DialogTrigger asChild>
+      <DialogTrigger className="flex " asChild>
         <Button
-          className="items-center justify-center flex font-medium text-lg text-[#5E6166] border-none"
+          className=" flex font-medium text-lg text-[#5E6166] border-none"
           variant="outline"
         >
           Create new category
@@ -23,7 +46,9 @@ export const AddCategory = () => {
       </DialogTrigger>
       <DialogContent className="w-[425px] bg-white">
         <DialogHeader>
-          <DialogTitle className="text-center">Create new category</DialogTitle>
+          <DialogTitle className="text-center flex items-center">
+            Create new category
+          </DialogTitle>
         </DialogHeader>
         <div className="grid gap-4 py-4">
           <div className="flex flex-col gap-4">
@@ -31,17 +56,21 @@ export const AddCategory = () => {
               Category name
             </Label>
             <Input
+              onChange={(e) => setName(e.target.value)}
               id="name"
               defaultValue=""
               className="col-span-3 h-[56px] w- bg-[#F4F4F4] rounded-xl w-full"
             />
           </div>
         </div>
-        <DialogFooter>
-          <Button className="text-white bg-[#393939]" type="submit">
-            continue
-          </Button>
-        </DialogFooter>
+        <Button
+          onClick={clickAddCategory}
+          className={`text-white  w-20 ml-auto ${
+            name ? "bg-green-500" : "bg-gray-700"
+          }`}
+        >
+          continue
+        </Button>
       </DialogContent>
     </Dialog>
   );
--- ui/src/components/Categories.tsx
+++ ui/src/components/Categories.tsx
@@ -1,19 +1,48 @@
-const name = ["Breakfast", "Soup", "Main Course", "Desserts"];
+"use client";
 
+import { useEffect, useState } from "react";
 import { AddCategory } from "./AddCategory";
 import { Category } from "./Category";
 import { Plus } from "./icons";
+import axios from "axios";
+
+interface Category {
+  _id: string;
+  categoryName: string;
+}
 
 export const Categories = () => {
+  const [categories, setCategories] = useState<Category[]>([]);
+  console.log(categories);
+
+  const getCategories = async () => {
+    try {
+      const response = await axios.get<Category[]>(
+        "http://localhost:8000/category"
+      );
+      setCategories(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getCategories();
+  }, []);
+
   return (
     <div className="w-[402px] h-[100%] bg-white ml-[40px]">
       <div className="mt-[26px] mb-10">
         <h1 className="font-bold text-2xl">Food Menu</h1>
       </div>
-      {name.map((text, index) => {
-        return <Category key={index} text={text} />;
-      })}
-      <div className="flex w-[258px] h-10 rounded-lg gap-2 border px-4 py-2 mb-[26px]">
+      {categories.length > 0 ? (
+        categories.map((category) => {
+          return <Category key={category._id} text={category.categoryName} />;
+        })
+      ) : (
+        <p>No categories available.</p> // Optionally handle the case where no categories exist
+      )}
+      <div className="flex w-[258px] items-center h-10 rounded-lg gap-2 border px-4 py-2 mb-[26px]">
         <Plus />
         <AddCategory />
       </div>
