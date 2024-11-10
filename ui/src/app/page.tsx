ce6a3fa3a8d74b42649ca88e13a671a71e1afaa5
Tuul10 <etuul186@gmial.com>
2024-11-10 18:29:30 +0800

    sheet duusaw

--- ui/src/app/page.tsx
+++ ui/src/app/page.tsx
@@ -2,11 +2,15 @@
 
 import { FastDelivery } from "@/components/FastDelivery";
 import { StarIcon } from "@/components/icons";
+import axios from "axios";
+import { useEffect } from "react";
+import { log } from "util";
 // import { useAuthcontext } from "@/providers/AuthProvider";
 // import { useRouter } from "next/navigation";
 // import { useEffect } from "react";
 
 const Home = () => {
+  const [foods, setFoods] = useState<Food[]>();
   // const { currentUser, isLoading } = useAuthcontext();
   // const router = useRouter();
 
@@ -17,6 +21,17 @@ const Home = () => {
   // }, [currentUser, isLoading, router]);
 
   // useEffect(() => {}, [router, currentUser, isLoading]);
+
+  const foods = async () => {
+    try {
+      const response = await axios.get("http://localhost:8000/food");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    foods();
+  }, []);
 
   return (
     <div className="container mx-auto  ">
